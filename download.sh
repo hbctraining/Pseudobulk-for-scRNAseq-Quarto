8c2afe47bc1268ad5e2fcd93cb618b13def984da
nsohail19 <20noor.sohail@gmail.com>
2025-06-16 14:13:21 -0400

    Lesson 1

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p data
+curl -L "https://www.dropbox.com/scl/fi/c3ggrdttuk3cqovqocy1a/Pseudobulk_workshop.zip?rlkey=nehku3i8mrtkbibe4wvt04n40&st=t8noj7w8&dl=1" -o Pseudobulk_workshop.zip
+unzip Pseudobulk_workshop.zip
+rm Pseudobulk_workshop.zip
+rm -rf __MACOSX
+
+mv Pseudobulk_workshop/data/BAT_GSE160585_final.rds data/BAT_GSE160585_final.rds
+rm -rf Pseudobulk_workshop
