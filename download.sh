4990ac480abb5b6e1ef03e9cad9ba9b094a68033
nsohail19 <20noor.sohail@gmail.com>
2025-06-16 16:24:55 -0400

    Day 1

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p data
+mkdir -p results
+
 curl -L "https://www.dropbox.com/scl/fi/c3ggrdttuk3cqovqocy1a/Pseudobulk_workshop.zip?rlkey=nehku3i8mrtkbibe4wvt04n40&st=t8noj7w8&dl=1" -o Pseudobulk_workshop.zip
 unzip Pseudobulk_workshop.zip
 rm Pseudobulk_workshop.zip
