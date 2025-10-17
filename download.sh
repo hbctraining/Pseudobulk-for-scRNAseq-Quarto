b423a2d95a8d0ce2deec5ffbaf0cc421729415d7
Gammerdinger <wjgdinger@gmail.com>
2025-10-17 06:17:11 -0400

    Updated schedule and lessons

--- download.sh
+++ download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p data
-mkdir -p results
+mkdir -p lessons/data
+mkdir -p lessons/results
 
 curl -L "https://www.dropbox.com/scl/fi/c3ggrdttuk3cqovqocy1a/Pseudobulk_workshop.zip?rlkey=nehku3i8mrtkbibe4wvt04n40&st=t8noj7w8&dl=1" -o Pseudobulk_workshop.zip
 unzip Pseudobulk_workshop.zip
 rm Pseudobulk_workshop.zip
 rm -rf __MACOSX
 
-mv Pseudobulk_workshop/data/BAT_GSE160585_final.rds data/BAT_GSE160585_final.rds
+mv Pseudobulk_workshop/data/BAT_GSE160585_final.rds lessons/data/BAT_GSE160585_final.rds
 rm -rf Pseudobulk_workshop
